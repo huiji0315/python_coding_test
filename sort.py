de80e972d2034f3a1e32540fde342ea0a0f23e72
huiji0315 <huiji0315@gmail.com>
2021-10-10 17:09:14 +0900

    sort 개념 응용 추가

--- sort.py
+++ sort.py
@@ -37,4 +37,23 @@ f = sorted(e, key = lamda x : (x[0], -x[1]))
 - 요소가 여러개일 경우 각 요소마다 정렬기준을 정해줄 수 있다. sorted(a, key=lambda x: (x[0], -x[1])
 
 참고) https://velog.io/@aonee/Python-%EC%A0%95%EB%A0%AC-sort-sorted-reverse
+
+4. dictionary 정렬
+ 1) 키를 기준으로 오름차순 정렬
+ - sorted(dict.keys()) : keys 만 정렬된 값을 반환
+ - sorted(dict.items()) : 키(key)를 기준으로 정렬하되 키와 값을 튜플로 묶어서 정렬된 값을 반환
+ - sorted(dict.items(), key=lambda item: len(item[0])) : 키의 길이(length)를 기준으로 오름차순 정렬
+ 2) 키를 기준으로 내림차순 정렬
+ : reverse=True를 추가해주면 된다.
+
+ 3) 값을 기준으로 오름차순 정렬
+ - sorted(dict.items(), key=lambda item: item[1])
+ 4) 값을 기준으로 내림차순 정렬
+ - sorted(dict.items(), reverse=True, key=lambda item: item[1])
+
+참고) https://rfriend.tistory.com/473
+
+[같이 참고하면 좋은 파일]
+- 211003 #10. 나이순 정렬
+- list&tuple&dictionary.py
 '''
